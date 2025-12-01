654d453c1761a4d4b3eb362cd33fd01d96b6da28
dasinua <dasinua@gmail.com>
2025-12-01 23:59:08 +0200

    button

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
 
 void main() {
-  runApp(const AdvBasics());
-}
-
-class AdvBasics extends StatelessWidget {
-  const AdvBasics({super.key});
-
-  @override
-  Widget build(context) {
-    return MaterialApp(
-      title: 'Adv Basics',
-      debugShowCheckedModeBanner: false,
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 54, 33, 243),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 250,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: Text('Start Quiz'),
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  side: BorderSide(color: Colors.white, width: 2),
-                ),
-              ),
-            ],
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 179, 117, 250),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
+          child: StartScreen(),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
